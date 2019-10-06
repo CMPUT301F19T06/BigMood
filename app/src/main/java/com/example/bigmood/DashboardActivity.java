e29132529e7cae7f7a3b779e01d22c4344bbb205
Jarrett Yu <jarrett.yu21@gmail.com>
2019-10-06 16:21:51 -0600

    added dashboard for real this time

---
+++ app/src/main/java/com/example/bigmood/DashboardActivity.java
@@ -0,0 +1,26 @@
+package com.example.bigmood;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class DashboardActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSideMenu();
+            }
+        });
+    }
+    private void startSideMenu() {
+        //TODO: trigger side menu fragment
+    }
+}
