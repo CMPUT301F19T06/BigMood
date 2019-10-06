b6d15c2a91dcdb3730b9073d8868897069971fc8
Jarrett Yu <jarrett.yu21@gmail.com>
2019-10-06 16:32:19 -0600

    added basic recyclerview

--- app/src/main/java/com/example/bigmood/DashboardActivity.java
+++ app/src/main/java/com/example/bigmood/DashboardActivity.java
@@ -4,21 +4,25 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        this.fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startSideMenu();
             }
         });
+        this.recyclerView = findViewById(R.id.dashboard_recyclerview);
     }
     private void startSideMenu() {
         //TODO: trigger side menu fragment
