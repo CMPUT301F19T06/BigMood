f236c4703eb533f67eb9639525d4a777af795ac3
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-10-31 22:10:26 -0600

    moodObject should now have most of it's parameters

--- app/src/main/java/com/example/bigmood/EditmoodActivity.java
+++ app/src/main/java/com/example/bigmood/EditmoodActivity.java
@@ -3,6 +3,7 @@ package com.example.bigmood;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +14,21 @@ public class EditmoodActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editmood);
 
+        //Get the moodObject out of the intent
         Intent intent = getIntent();
         moodObject mood = (moodObject) intent.getSerializableExtra("mood");
+
+        //Create the toolbar object and set it to the action bar setSupportAction
+        //Create mood setter
+        //Create color setter
+        //Create picture setter
+
+        //Set up the Save Button
+        findViewById(R.id.button_editmood_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bigmood/moodObject.java
+++ app/src/main/java/com/example/bigmood/moodObject.java
@@ -1,15 +1,38 @@
 package com.example.bigmood;
 
+import android.app.Activity;
+
 import java.io.Serializable;
+import java.util.Date;
+
 
 public class moodObject implements Serializable {
-    private String mood;
+
+    private String moodType; //The type of mood
+
+    private String moodDescription;
+    private int moodColor;
+    private Date moodDate;
 
     public moodObject (String mood) {
-        this.mood = mood;
+        this.moodType = mood;
     }
 
-    public String mood(){
-        return this.mood;
+    public moodObject (String moodType, String moodDescription, int moodColor, Date moodDate){
+        this.moodType = moodType;
+        this.moodDescription = moodDescription;
+        this.moodColor = moodColor;
+        this.moodDate = moodDate;
     }
+
+    public String getMoodType(){ return this.moodType; }
+
+    public String getMoodDescription() { return moodDescription; }
+    public void setMoodDescription(String moodDescription) { this.moodDescription = moodDescription; }
+
+    public int getMoodColor() { return moodColor; }
+    public void setMoodColor(int moodColor) { this.moodColor = moodColor; }
+
+    public Date getMoodDate() { return moodDate; }
+    public void setMoodDate(Date moodDate) { this.moodDate = moodDate; }
 }
