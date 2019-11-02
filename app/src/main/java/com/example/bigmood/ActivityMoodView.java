30661c841bd680b94959aa42d72e1754377acd07
Nasif <nhossain@ualberta.ca>
2019-11-02 17:04:52 -0600

    test

---
+++ app/src/main/java/com/example/bigmood/ActivityMoodView.java
@@ -0,0 +1,14 @@
+package com.example.bigmood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMoodView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_view);
+    }
+}
