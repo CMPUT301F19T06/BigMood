a8952e15407ffaaf4fe6e40e7e4d6d3bcf63a711
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-08 12:52:56 -0700

    Added the javadoc s for the Mood and User

--- app/src/main/java/com/example/bigmood/Mood.java
+++ app/src/main/java/com/example/bigmood/Mood.java
@@ -5,22 +5,55 @@ import android.app.Activity;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * This is the data structure for the mood information. It implements serializable so that it can be passed
+ * as an extra through intents.
+ */
 
 public class Mood implements Serializable {
 
-    private String moodDescription;
-    private String moodColor;
-    private Date moodDate;
-    private String moodID;
+    /**
+     * The main type of the mood, such as Angry or Happy
+     */
     private String moodTitle;
+    /**
+     * The description of the mood, a personal line
+     */
+    private String moodDescription;
+    /**
+     * The hex code for the color, as a string
+     */
+    private String moodColor;
+    /**
+     * The date of the mood event
+     */
+    private Date moodDate;
+    /**
+     * The unique Identifier for the mood
+     */
+    private String moodID;
+    /**
+     * The link to the moodPhoto
+     */
     private String moodPhoto;
+    /**
+     * The name of the representative emoji
+     */
     private String moodEmoji;
 
+    /**
+     * This is the empty constructor, this will represent the default Mood
+     * TODO:Construct default
+     */
     public Mood(){}
 
-    public Mood (String mood) {
-        this.moodTitle = mood;
-    }
+    /**
+     * This constructor is where all parameters are passed in and set in the object.
+     * @param moodTitle
+     * @param moodDescription
+     * @param moodColor
+     * @param moodDate
+     */
 
     public Mood (String moodTitle, String moodDescription, String moodColor, Date moodDate){
         this.moodTitle = moodTitle;
@@ -29,53 +62,101 @@ public class Mood implements Serializable {
         this.moodDate = moodDate;
     }
 
+    /**
+     * Get the mood title
+     * @return
+     * The mood title
+     */
     public String getMoodTitle() {
         return moodTitle;
     }
 
+    /**
+     * Set the mood title
+     * @param moodTitle
+     * The title to be set to
+     */
     public void setMoodTitle(String moodTitle) {
         this.moodTitle = moodTitle;
     }
-
+    /**
+     * Get the mood photo
+     * @return
+     * The mood photo
+     */
     public String getMoodPhoto() {
         return moodPhoto;
     }
-
+    /**
+     * Set the mood photo
+     * @param moodPhoto
+     * The photo to be set to
+     */
     public void setMoodPhoto(String moodPhoto) {
         this.moodPhoto = moodPhoto;
     }
-
+    /**
+     * Get the mood emoji
+     * @return
+     * The mood emoji
+     */
     public String getMoodEmoji() {
         return moodEmoji;
     }
-
+    /**
+     * Set the mood Emoji
+     * @param moodEmoji
+     * The emoji to be set to
+     */
     public void setMoodEmoji(String moodEmoji) {
         this.moodEmoji = moodEmoji;
     }
-
+    /**
+     * Get the mood ID
+     * @return
+     * The mood ID
+     */
     public String getMoodID() {
         return moodID;
     }
-
+    /**
+     * Set the mood ID
+     * @param moodID
+     * The ID to be set to
+     */
     public void setMoodID(String moodID) {
         this.moodID = moodID;
     }
-
-
+    /**
+     * Get the mood description
+     * @return
+     * The mood description
+     */
     public String getMoodDescription() {
         return moodDescription;
     }
+    /**
+     * Set the mood Description
+     * @param moodDescription
+     * The Description to be set to
+     */
     public void setMoodDescription(String moodDescription) {
         this.moodDescription = moodDescription;
     }
 
     /**
-     * Had to make this a string or else no use
+     * Get the mood color
      * @return
+     * The mood color. Had to make this a string or else it is currently not useable
      */
     public String getMoodColor() {
         return moodColor;
     }
+    /**
+     * Set the mood Color
+     * @param moodColor
+     * The Color to be set to
+     */
     public void setMoodColor(String moodColor) {
         this.moodColor = moodColor;
     }
@@ -83,6 +164,11 @@ public class Mood implements Serializable {
     public Date getMoodDate() {
         return moodDate;
     }
+    /**
+     * Set the mood Date
+     * @param moodDate
+     * The Date object to be set to
+     */
     public void setMoodDate(Date moodDate) {
         this.moodDate = moodDate;
     }
