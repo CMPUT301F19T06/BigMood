685aa42d26695010f07c10d9203fb7be1940d556
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-11 17:27:10 -0700

    Filling out the requirements for the activity.

--- app/src/main/java/com/example/bigmood/GpsActivity.java
+++ app/src/main/java/com/example/bigmood/GpsActivity.java
@@ -8,18 +8,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 /**
  * The GpsActivity
  *
- * This activity is called from the sidebar and from the userview
+ * This activity is called from the sidebar and from the userview to display mood locations dependent
+ * on the mode of operation.
  *
  * Passed in through intent:
+ *
  * "USER_ID" :  the id of the current user
+ *
+ * "MODE" : A String choosing the mode in which the activity is initially called in:
+ *  - "USER": Show the moods of the user's history
+ *  - "FOLLOW": Show the moods of those that the user is following
  */
 
 public class GpsActivity extends AppCompatActivity {
 
-    String userId;
+    String userId; //The Current user's id
+    String mode; // The mode of operation
+
+    /*
+    The arraylist for the user's moods, initially null and only loaded from the database if
+    the mode is set to "USER" and it is still null
+     */
+    ArrayList<Mood> userMoods = null;
+
+    /*
+    The arraylist holding the followed moods, this will need to be refreshed every time
+    that "FOLLOW" mode is selected.
+     */
+    ArrayList<Mood> followedMoods;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +48,14 @@ public class GpsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gps);
 
         this.userId = getIntent().getExtras().getString("USER_ID");
+        this.mode = getIntent().getExtras().getString("MODE");
+
+        if (this.mode.equals("USER")){
+            retrieveUserMoods();
+        }
+        else{
+            retrieveFollowedMoods();
+        }
 
         FloatingActionButton modeButton = findViewById(R.id.gps_button_mode);
         FloatingActionButton zoominButton = findViewById(R.id.gps_button_zoomin);
@@ -42,7 +71,7 @@ public class GpsActivity extends AppCompatActivity {
         zoominButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: When th zoom in button is pressed
+                //TODO: When the zoom in button is pressed
             }
         });
 
@@ -54,5 +83,13 @@ public class GpsActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void retrieveUserMoods(){
+        //TODO: Retrieve the users moods
+    }
+
+    private void retrieveFollowedMoods(){
+        //TODO: retrieve followed moods
     }
 }
