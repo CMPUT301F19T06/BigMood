43fb94940a15a1aa0f04231bcfc6ca3c60b9791c
HH-NOTEBOOK\henry <henry.huang@tnactivecare.com>
2019-11-13 14:47:50 -0700

    Implemented the start of friends, because you guys don't have any friends.
    Also fixed a bit of corruption in files (???)

---
+++ app/src/main/java/com/example/bigmood/FriendsRecyclerViewAdapter.java
@@ -0,0 +1,99 @@
+package com.example.bigmood;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+//Here, we're using a RecyclerViewAdapter instead of a Listylist due to the limitations
+public class FriendsRecyclerViewAdapter extends RecyclerView.Adapter<FriendsRecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+    private static final String FRIEND_ID = "com.example.bigmood.FriendRecycleViewAdapter";
+
+    //set up local arraylist to store rides
+    //private ArrayList<Mood> moodIDs = new ArrayList<>();
+
+    //set up interface components to measure input from clicks
+    private Context mContext;
+    private String userId;
+    ImageView deleteMood;
+    //constructor
+    public FriendsRecyclerViewAdapter(ArrayList friendIDs, String userId, Context mContext) {
+        FriendsActivity.friendObjects = friendIDs;
+        this.mContext = mContext;
+        this.userId = userId;
+    }
+
+    //set up a new viewholder to mount onto main activity
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_friendfragment, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    //bind viewholder to holder, process input here
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        //prepare display variables for list
+        Log.d(TAG, "onBindViewHolder: called.");
+        FriendsActivity.index = position;
+
+        //set up the connection to view here, TBA
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on:" + FriendsActivity.friendObjects.get(position));
+                //intentMoodView(FriendsActivity.moodObjects.get(position), v);
+            }
+        });
+        //establish listener for each element
+
+    }
+
+    public void intentMoodView(Mood moodID, View v){
+        //Intent intent = new Intent(v.getContext(), ActivityAddMood.class);
+        //intent.putExtra("Mood", moodID);
+        //intent.putExtra("USER_ID", this.userId);
+        //mContext.startActivity(intent);
+    }
+
+    //item count for parsing through
+    @Override
+    public int getItemCount() {
+        return FriendsActivity.friendObjects.size();
+    }
+
+    //constructor for ViewHolder object, which holds our xml components together
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        // TextView text;
+        TextView friendName;
+        ConstraintLayout linearLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // establish views here
+            friendName = itemView.findViewById(R.id.friendName);
+        }
+    }
+}
